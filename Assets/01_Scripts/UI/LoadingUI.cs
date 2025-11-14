9c384c7452e3f4f831df5cefbd946db2bd1f62f8
shin0112 <wnslcosltimo12@gmail.com>
2025-11-14 23:26:18 +0900

    feat: 로딩 UI 만들어서 Scene 넘어갈 때마다 동작하게 하기

---
+++ Assets/01_Scripts/UI/LoadingUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingUI : MonoBehaviour
+{
+    [SerializeField] private Image _progress;
+    [SerializeField] private Image _character;
+
+    public void LoadScene(SceneType type)
+    {
+        StartCoroutine(LoadSceneCoroutine(type));
+    }
+
+    private IEnumerator LoadSceneCoroutine(SceneType type)
+    {
+        _progress.fillAmount = 0f;
+        float progress = 0f;
+        float originY = _character.rectTransform.position.y;
+        AsyncOperation async = SceneManager.LoadSceneAsync(type.ToString());
+
+        while (!async.isDone)
+        {
+            progress = async.progress / 0.95f;
+            _progress.fillAmount = Mathf.Clamp01(progress);
+            float x = Mathf.Lerp(0f, Define.ProgressCharacterEndX, progress);
+            _character.rectTransform.position = new Vector2(x, originY);
+            yield return null;
+        }
+
+        _progress.fillAmount = 1f;
+        _character.rectTransform.position = Vector2.right * Define.ProgressCharacterEndX;
+
+        gameObject.SetActive(false);
+    }
+}
