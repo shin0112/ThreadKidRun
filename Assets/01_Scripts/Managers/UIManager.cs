1988402d082f8a29494cfe6dc2ea608ac2f30b86
shin0112 <wnslcosltimo12@gmail.com>
2025-11-14 23:27:03 +0900

    feat: SceneType enum 작성, 로딩 UI 추가, LoadScene 메서드 제작

--- Assets/01_Scripts/Managers/UIManager.cs
+++ Assets/01_Scripts/Managers/UIManager.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
 
+public enum SceneType
+{
+    LoadScene,
+    GameScene,
+    StartScene,
+}
+
 public class UIManager : MonoBehaviour
 {
     private static UIManager _instance;
     public static UIManager Instance => _instance;
+
+    [SerializeField] private LoadingUI _loading;
 
     private void Awake()
     {
@@ -15,5 +24,20 @@ public class UIManager : MonoBehaviour
 
         _instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        LoadScene(SceneType.StartScene);
+    }
+
+    /// <summary>
+    /// 씬 전환하기
+    /// </summary>
+    /// <param name="type"></param>
+    public void LoadScene(SceneType type)
+    {
+        _loading.gameObject.SetActive(true);
+        _loading.LoadScene(type);
     }
 }
