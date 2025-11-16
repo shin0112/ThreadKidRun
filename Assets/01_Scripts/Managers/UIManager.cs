df43a69919ef310f710d49f5da45153adfdcbb67
shin0112 <wnslcosltimo12@gmail.com>
2025-11-16 22:45:49 +0900

    feat: UIManager.cs 수정 (버튼, ui 연결, 버튼 listener 추가)
    - setting, store button 연결
    - setting ui go(canvas) 연결
    - button 리스너 추가/제거

--- Assets/01_Scripts/Managers/UIManager.cs
+++ Assets/01_Scripts/Managers/UIManager.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     private static UIManager _instance;
     public static UIManager Instance => _instance;
+
+    [Header("아이콘 버튼")]
+    [SerializeField] private Button _settingButton;
+    [SerializeField] private Button _storeButton;
+
+    [Header("UI")]
+    [SerializeField] private GameObject _settingUI;
 
     private void Awake()
     {
@@ -15,5 +23,23 @@ public class UIManager : MonoBehaviour
 
         _instance = this;
         DontDestroyOnLoad(gameObject);
+
+        _settingUI.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        _settingButton.onClick.AddListener(ToggleSettingUI);
+    }
+
+    private void OnDisable()
+    {
+        _settingButton.onClick.RemoveAllListeners();
+        _storeButton.onClick.RemoveAllListeners();
+    }
+
+    private void ToggleSettingUI()
+    {
+        _settingUI.Toggle();
     }
 }
