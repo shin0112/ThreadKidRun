8eff6cb5ba62c87514fba0606312d1b1c74955f6
shin0112 <wnslcosltimo12@gmail.com>
2025-11-17 20:25:28 +0900

    feat: 현재 선택 중인 캐릭터 번호 저장

--- Assets/01_Scripts/Shop/ShopController.cs
+++ Assets/01_Scripts/Shop/ShopController.cs
@@ -3,21 +3,38 @@ using UnityEngine;
 
 public class ShopController : MonoBehaviour
 {
+    // 컴포넌트
     [SerializeField] private Transform _characterContainer;
     [SerializeField] private List<GameObject> _characters;
     [SerializeField] private Camera _shopCamera;
-
     public Camera ShopCamera => _shopCamera;
+
+    // 캐릭터 리스트 관리
+    private int _curSelectIndex = 0;
+
+    private void Awake()
+    {
+        if (_characters.Count != Define.CharacterCount)
+        {
+            Logger.Log("캐릭터 개수 불일치");
+        }
+    }
 
     public void RotateRight()
     {
         Vector3 rotation = _characterContainer.localEulerAngles + new Vector3(0, 60f, 0f);
         _characterContainer.localEulerAngles = rotation;
+
+        _curSelectIndex = (_curSelectIndex + 1) % Define.CharacterCount;
+        Logger.Log($"현재 캐릭터 번호: {_curSelectIndex}");
     }
 
     public void RotateLeft()
     {
         Vector3 rotation = _characterContainer.localEulerAngles + new Vector3(0, -60f, 0f);
         _characterContainer.localEulerAngles = rotation;
+
+        _curSelectIndex = (_curSelectIndex + Define.CharacterCount - 1) % Define.CharacterCount;
+        Logger.Log($"현재 캐릭터 번호: {_curSelectIndex}");
     }
 }
--- Assets/01_Scripts/Utils/Define.cs
+++ Assets/01_Scripts/Utils/Define.cs
@@ -3,4 +3,8 @@ public static class Define
     #region UI
     public static int ProgressCharacterEndX = 1730;
     #endregion
+
+    #region Shop
+    public static int CharacterCount = 6;
+    #endregion
 }
