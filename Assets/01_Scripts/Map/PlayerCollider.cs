15aed48defd29a82590e50a5e265740af18f3f24
yohan <dldygksp9701@gmail.com>
2025-11-18 19:54:04 +0900

    feat 빛 어둠 구현 (미완성)

--- Assets/01_Scripts/Map/PlayerCollider.cs
+++ Assets/01_Scripts/Map/PlayerCollider.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCollider : MonoBehaviour
 {
     public MapMove mapMove;
-    
+    public GameObject gameOverUi;
     void Start()
     {
-        Ray ray = new Ray(transform.position, Vector3.forward * 0.5f);
+        //Ray ray = new Ray(transform.position, Vector3.forward * 0.5f);
     }
 
 
@@ -23,17 +24,17 @@ public class PlayerCollider : MonoBehaviour
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             mapMove.isMove = false;
-            Debug.Log(mapMove.isMove);
-            //StartCoroutine(nameof(CollderGameOver));
+            gameOverUi.SetActive(true);
+            StartCoroutine(nameof(CollderGameOver));
         }
     }
     
 
     IEnumerator CollderGameOver()
     {
-        yield return new WaitForSeconds(0f);
-
-        //Time.timeScale = 0f;
+        yield return new WaitForSeconds(1f);
+        
+        Time.timeScale = 0f;
     }
 }
 //플레이어가 태그 obstacle과 부딪쳤을 때
