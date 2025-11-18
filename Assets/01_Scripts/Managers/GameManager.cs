5b86dfc3ad7031ab1097c5c8e7e9d148d6bccb67
Cafor <formuloratio@gmail.com>
2025-11-18 12:21:17 +0900

    refactor: 업적 해금 타입 범위 확장 및 코인/스코어 분리

--- Assets/01_Scripts/Managers/GameManager.cs
+++ Assets/01_Scripts/Managers/GameManager.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("코인 저장 값 초기화")]
+    public bool isReset; //디버깅용
+
     public static GameManager Instance { get; private set; }
 
     // 현재 획득한 총 코인 개수
     private int totalCoinCount = 0;
-    public int TotalCoinCount => totalCoinCount;
+
+    //현재 스코어
+    private int currentScore = 0;
 
     [SerializeField]
     private AchievementManager achievementManager;
@@ -34,23 +39,36 @@ public class GameManager : MonoBehaviour
         LoadData();
     }
 
+    public void AddScore(int amount)
+    {
+        if (amount <= 0) return;
+        currentScore += amount;
+        OnScoreChanged?.Invoke(currentScore);
+        EarnCoin(amount);
+        UnityEngine.Debug.Log($"코인 획득! 현재 스코어: {currentScore}");
+    }
+
     // 코인을 획득할 때 호출되는 메서드
     public void EarnCoin(int amount)
     {
         if (amount <= 0) return;
         totalCoinCount += amount;
-        OnScoreChanged?.Invoke(totalCoinCount);
         UnityEngine.Debug.Log($"코인 획득! 현재 총 코인: {totalCoinCount}");
 
         // 코인 개수가 변경될 때마다 업적 해금 조건을 검사
         if (achievementManager != null)
         {
-            achievementManager.CheckAchievements(totalCoinCount);
-        }
+            achievementManager.CheckAchievements(totalCoinCount, AchievementType.CoinAcquisition);
+        } // <- 업적 해금 방법. 다른 업적(아이템 사용, 아이템 구매하는) 함수에 위의 코드를 가져다 쓰면 됨
     }
 
     private void LoadData()
     {
+        if (isReset)
+        {
+            totalCoinCount = 0;
+            return;
+        }
         totalCoinCount = PlayerPrefs.GetInt("CurrentCoin", 0);
     }
 
