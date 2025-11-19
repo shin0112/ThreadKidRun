b915c00d205c29ed610baae93b2e8062bfaeb670
shin0112 <wnslcosltimo12@gmail.com>
2025-11-19 13:05:30 +0900

    feat: ShopUI 스크립트, 프리팹 변경
    - 구매 버튼 제작

--- Assets/01_Scripts/UI/ShopUI.cs
+++ Assets/01_Scripts/UI/ShopUI.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,6 +6,8 @@ public class ShopUI : MonoBehaviour, IUIActive
 {
     [SerializeField] private Button _leftArrow;
     [SerializeField] private Button _rightArrow;
+    [SerializeField] private Button _get;
+    [SerializeField] private TextMeshProUGUI _price;
 
     ShopController _controller;
 
@@ -19,12 +22,23 @@ public class ShopUI : MonoBehaviour, IUIActive
 
         _leftArrow.onClick.AddListener(_controller.RotateLeft);
         _rightArrow.onClick.AddListener(_controller.RotateRight);
+        // todo: get 버튼 구현
+
+        _controller.OnChangedPriceText += UpdatePriceText;
     }
 
     private void OnDestroy()
     {
         _leftArrow.onClick.RemoveAllListeners();
         _rightArrow.onClick.RemoveAllListeners();
+        _get.onClick.RemoveAllListeners();
+
+        _controller.OnChangedPriceText -= UpdatePriceText;
+    }
+
+    private void UpdatePriceText(int price)
+    {
+        _price.text = price.ToString();
     }
 
     #region 인터페이스 구현
@@ -32,18 +46,19 @@ public class ShopUI : MonoBehaviour, IUIActive
     {
         _leftArrow.gameObject.SetActive(false);
         _rightArrow.gameObject.SetActive(false);
+        _get.gameObject.SetActive(false);
         _controller.ShopCamera.gameObject.SetActive(false);
     }
 
     public void SetGameMode()
     {
-        gameObject.SetActive(false);
     }
 
     public void SetShopMode()
     {
         _leftArrow.gameObject.SetActive(true);
         _rightArrow.gameObject.SetActive(true);
+        _get.gameObject.SetActive(true);
         _controller.ShopCamera.gameObject.SetActive(true);
     }
     #endregion
