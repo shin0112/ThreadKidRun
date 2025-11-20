e8a12dd962db9e02ec39d98a23bafb61d043f214
Cafor <formuloratio@gmail.com>
2025-11-20 14:20:09 +0900

    feart: 일시정지 버튼 클릭 시 멈춤

--- Assets/01_Scripts/UI/PauseUI.cs
+++ Assets/01_Scripts/UI/PauseUI.cs
@@ -15,12 +15,14 @@ public class PauseUI : MonoBehaviour
     {
         _continue.onClick.AddListener(ContinueGame);
         _exit.onClick.AddListener(ExitGame);
+        Time.timeScale = 0f;
     }
 
     private void OnDisable()
     {
         _continue.onClick.RemoveAllListeners();
         _exit.onClick.RemoveAllListeners();
+        Time.timeScale = 1f;
     }
 
     public void UpdateCurrentScoreText(int value)
